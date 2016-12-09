e3b06fdbe0642f0dcfa816d9937484bafd17e40f
Dale Stone <dale.stone@epicor.com>
2016-12-09 11:01:53 +0000

    Added reference to published lib package

--- src/demo-app/demo-app.module.ts
+++ src/demo-app/demo-app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { DemoAppComponent }   from './demo-app.component';
-import { GanttModule } from '../lib'; //TODO(dale): import "angular2-gantt"
+import { GanttModule  } from 'ng2-gantt';
 
 @NgModule({
     imports: [
