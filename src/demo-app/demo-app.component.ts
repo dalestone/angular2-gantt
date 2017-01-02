2517d8c0ba433658851dc985101bffc77af05eac
Dale Stone <dale@stonenet.co.uk>
2017-01-02 18:19:23 +0000

    Updated project data to test vertical scroll

--- src/demo-app/demo-app.component.ts
+++ src/demo-app/demo-app.component.ts
@@ -7,49 +7,84 @@ import { Project, Options } from 'ng2-gantt';
 })
 export class DemoAppComponent {
 
-    constructor() {}
+    constructor() { }
 
     // Default options
-    options: Options = {                
+    options: Options = {
         scale: {
             start: new Date(2017, 1, 1),
             end: new Date(2017, 2, 30)
         }
     };
 
-    project: Project =  {
-                'id': 'dd10f0b6-b8a4-4b2d-a7df-b2c3d63b4a01',
-                'name': 'Angular2 Gantt',
-                'tasks': [
-                    {
-                        'id': '1', 
-                        'name':'Lorem ipsum dolor sit amet.',
-                        'resource': 'res1',
-                        'start': new Date('2017-02-10T00:00:00.0Z'),
-                        'end': new Date('2017-02-22T00:00:00.0Z')
-                    },
-                    {
-                        'id': '2',
-                        'name': 'Praesent molestie lobortis mi non tempor.',
-                        'resource': 'res1',
-                        'start': new Date('2017-02-22T00:00:00.0Z'),
-                        'end': new Date('2017-02-23T00:00:00.0Z')
-                    },
-                    {
-                        'id': '3',
-                        'name': 'Cras sollicitudin egestas velit sit amet aliquam.',
-                        'resource': 'res2',
-                        'start': new Date('2017-02-23T00:00:00.0Z'),
-                        'end': new Date('2017-02-24T00:00:00.0Z')
-                    },
-                    {
-                        'id': '4',
-                        'name': 'Donec ac augue est.',
-                        'resource': 'res2',
-                        'start': new Date('2017-02-01T00:00:00.0Z'),
-                        'end': new Date('2017-02-26T00:00:00.0Z')
-                    },
-                ]
-            };
+    project: Project = {
+        'id': 'dd10f0b6-b8a4-4b2d-a7df-b2c3d63b4a01',
+        'name': 'Angular2 Gantt',
+        'tasks': [
+            {
+                'id': '1',
+                'name': 'Lorem ipsum dolor sit amet.',
+                'resource': 'res1',
+                'start': new Date('2017-02-01T00:00:00.0Z'),
+                'end': new Date('2017-02-03T00:00:00.0Z')
+            },
+            {
+                'id': '2',
+                'name': 'Praesent molestie lobortis mi non tempor.',
+                'resource': 'res1',
+                'start': new Date('2017-02-03T00:00:00.0Z'),
+                'end': new Date('2017-02-05T00:00:00.0Z')
+            },
+            {
+                'id': '3',
+                'name': 'Cras sollicitudin egestas velit sit amet aliquam.',
+                'resource': 'res2',
+                'start': new Date('2017-02-05T00:00:00.0Z'),
+                'end': new Date('2017-02-06T00:00:00.0Z')
+            },
+            {
+                'id': '4',
+                'name': 'Donec ac augue est.',
+                'resource': 'res2',
+                'start': new Date('2017-02-06T00:00:00.0Z'),
+                'end': new Date('2017-02-07T00:00:00.0Z')
+            },
+            {
+                'id': '5',
+                'name': 'Lorem ipsum dolor sit amet.',
+                'resource': 'res1',
+                'start': new Date('2017-02-07T00:00:00.0Z'),
+                'end': new Date('2017-02-22T00:00:00.0Z')
+            },
+            {
+                'id': '6',
+                'name': 'Praesent molestie lobortis mi non tempor.',
+                'resource': 'res1',
+                'start': new Date('2017-02-22T00:00:00.0Z'),
+                'end': new Date('2017-02-23T00:00:00.0Z')
+            },
+            {
+                'id': '7',
+                'name': 'Cras sollicitudin egestas velit sit amet aliquam.',
+                'resource': 'res2',
+                'start': new Date('2017-02-23T00:00:00.0Z'),
+                'end': new Date('2017-02-24T00:00:00.0Z')
+            },
+            {
+                'id': '8',
+                'name': 'Donec ac augue est.',
+                'resource': 'res2',
+                'start': new Date('2017-02-24T00:00:00.0Z'),
+                'end': new Date('2017-02-26T00:00:00.0Z')
+            },
+            {
+                'id': '9',
+                'name': 'Donec ac augue est.',
+                'resource': 'res2',
+                'start': new Date('2017-02-25T00:00:00.0Z'),
+                'end': new Date('2017-02-26T00:00:00.0Z')
+            },
+        ]
+    };
 
 }
